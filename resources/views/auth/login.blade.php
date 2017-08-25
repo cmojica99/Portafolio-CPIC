7b4f961c2b2213a3fbefd646e44f3725c34f3aa5
paolasanchezq <cpsanchez37@misena.edu.co>
2017-08-25 16:58:17 -0500

    Cambios

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <button type="button" class="close" data-dismiss="modal"><span class="fa fa-times" aria-hidden="true">&times;</span></button>
         <h4 class="modal-title text-center" id="myModalLabel">INICIAR SESIÓN</h4>
     </div>
     <div class="modal-body">
@@ -10,7 +10,7 @@
         {{ csrf_field() }}
 
         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-            <label for="email" class="col-md-4 control-label">Correo electronico</label>
+            <label for="email" class="col-md-4 control-label"><i class="icono-ema fa fa-at"></i>  Correo electronico</label>
 
             <div class="col-md-6">
                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
@@ -24,7 +24,7 @@
         </div>
 
         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-            <label for="password" class="col-md-4 control-label">Contraseña</label>
+            <label for="password" class="col-md-4 control-label"><i class="icono-pass fa fa-unlock-alt"></i>  Contraseña</label>
 
             <div class="col-md-6">
                 <input id="password" type="password" class="form-control" name="password" required>
